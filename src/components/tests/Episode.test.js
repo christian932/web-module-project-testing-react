46e865b2d86ecea0ba6834cef7185a691fc0a30b
christian932 <christianfarnsworth@gmail.com>
2021-09-28 18:39:51 -0700

    done

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,24 +8,45 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "yeet",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
+    id:2,
+    name: "",
+    image: "",
+    season: 1,
+    number: 2,
+    summary: "yeet",
+    runtime: 1
 }
 
 test("renders without error", () => {
-
+    render(<Episode episode={testEpisode}/>)
 });
 
 test("renders the summury test passed as prop", ()=>{
-    
+    render(<Episode episode={testEpisode}/>);
+
+    const summary = screen.queryAllByText(/yeet/i);
+
+    expect(summary).toHaveLength(1);
 });
 
 test("renders default image when image is not defined", ()=>{
-    
+    render(<Episode episode={testEpisodeWithoutImage}/>);
+
+    const testData = testEpisodeWithoutImage
+
+    const img = screen.getByAltText('./stranger_things.png');
+    if(img === './stranger_things.png'){
+       testData.image = 'http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg'
+    }else {
+       testData.image = "./stranger_things.png"
+    }
+
+    expect(img.src).toContain('http://localhost/stranger_things.png')
 })
 
 //Tasks
